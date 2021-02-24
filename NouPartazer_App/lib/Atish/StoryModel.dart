7a334eff3ab337d9cdd009cc0d546b9d16c794c7
atishdhu <atish-dhunnoo@hotmail.com>
2021-02-24 17:25:43 +0400

    ➕ Add Scaffold

--- NouPartazer_App/lib/Atish/StoryModel.dart
+++ NouPartazer_App/lib/Atish/StoryModel.dart
@@ -6,6 +6,7 @@ import './TagsTab.dart';
 
 class StoryModel extends StatelessWidget
 {
+  int current = 0;
   final List<Story> stories =
   [
     Story
@@ -45,95 +46,97 @@ class StoryModel extends StatelessWidget
   @override
   Widget build(BuildContext context)
   {
-    return Container(
-      height: 800,
-      child: SingleChildScrollView
-      (
-        scrollDirection: Axis.horizontal,
-        child: Row
+    return Scaffold(
+        body: Container(
+        height: 800,
+        child: SingleChildScrollView
         (
-          children:
-            stories.map((st) 
-            {
-              return Card
-              (
-                margin: EdgeInsets.fromLTRB(30, 80, 30, 30),
-                child: Container
+          scrollDirection: Axis.horizontal,
+          child: Row
+          (
+            children:
+              stories.map((st) 
+              {
+                return Card
                 (
-                  height: 700,
-                  width: 350,
-                  decoration: BoxDecoration
+                  margin: EdgeInsets.fromLTRB(30, 80, 30, 30),
+                  child: Container
                   (
-                    image: DecorationImage
+                    height: 700,
+                    width: 350,
+                    decoration: BoxDecoration
                     (
-                      image: AssetImage(st.image),
-                      fit: BoxFit.cover,
-                    )
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20,5,10,20),
-                    child: Column
-                    (
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: 
-                      [
-                        Text
-                        ( 
-                          st.locationName,
-                          style: TextStyle
-                          (
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                            color: new Color.fromRGBO(245, 197, 41, 1),
-                          ),
-                        ),
-                        Text
-                        (
-                          st.locationAddress,
-                          style: TextStyle
+                      image: DecorationImage
+                      (
+                        image: AssetImage(st.image),
+                        fit: BoxFit.cover,
+                      )
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20,5,10,20),
+                      child: Column
+                      (
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: 
+                        [
+                          Text
+                          ( 
+                            st.locationName,
+                            style: TextStyle
                             (
                               fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                              color: Colors.white,
-                            ),
-                        ),
-                        Row
-                        (
-                          children: 
-                          [
-                            InfoTab
-                            (
-                              icon: Icons.calendar_today_outlined, 
-                              date: st.date
-                            ),
-                            
-                            InfoTab
-                            (
-                              icon: Icons.query_builder_outlined, 
-                              date: st.time
-                            ),
-                          ],
-                        ),
-                        SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Container(
-                            width: 680,
-                            child: Row(
-                              children: [
-                                TagsTab(data: st.tag),
-                                TagsTab(data: st.foodType),
-                                TagsTab(data: st.foodType),
-                              ],
+                              fontSize: 25,
+                              color: new Color.fromRGBO(245, 197, 41, 1),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  )
-                ),
-              );
-            }).toList(),
+                          Text
+                          (
+                            st.locationAddress,
+                            style: TextStyle
+                              (
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                                color: Colors.white,
+                              ),
+                          ),
+                          Row
+                          (
+                            children: 
+                            [
+                              InfoTab
+                              (
+                                icon: Icons.calendar_today_outlined, 
+                                date: st.date
+                              ),
+                              
+                              InfoTab
+                              (
+                                icon: Icons.query_builder_outlined, 
+                                date: st.time
+                              ),
+                            ],
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Container(
+                              width: 680,
+                              child: Row(
+                                children: [
+                                  TagsTab(data: st.tag),
+                                  TagsTab(data: st.foodType),
+                                  TagsTab(data: st.foodType),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ),
+                );
+              }).toList(),
+          ),
         ),
       ),
     );
