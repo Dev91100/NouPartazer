0a6037e636bf543cdadc814f4f607117e37e4c3c
atishdhu <atish-dhunnoo@hotmail.com>
2021-02-25 18:13:56 +0400

    🧪 #2 Test responsiveness

--- NouPartazer_App/lib/SplashScreen.dart
+++ NouPartazer_App/lib/SplashScreen.dart
@@ -26,129 +26,163 @@ class _SpashScreenState extends State<SpashScreen> {
   Widget build(BuildContext context) {
     final screen = MediaQuery.of(context).size;
 
-    return Scaffold(
-      body: Stack(
-        children: 
-        [
-          Container
+    return LayoutBuilder
+    (
+      builder: (ctx, constraints)
+      {
+        return Scaffold
+        (
+          body: Stack
           (
-            height: screen.height,
-            decoration: BoxDecoration
-            (
-              image: DecorationImage
+            children: 
+            [
+              Container
               (
-                image: AssetImage('assets/JPEG/SplashScreen.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            // SpinKitSquareCircle(color:Colors.orangeAccent,size: 25.0,),
-          ),
-          Container
-          (
-            color: Color.fromRGBO(0, 0, 0, 0.3)
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 200, 0, 0),
-            width: double.infinity,
-            child: Column
-            (
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children:
-              [
-                Image.asset
+                height: constraints.maxHeight,
+                decoration: BoxDecoration
                 (
-                  'assets/PNG/NouPartazer LogoOnly_Large.png',
-                  height: screen.height * 0.2,
-                  fit: BoxFit.cover,
-                ),
-                SizedBox
-                (
-                  height: screen.height * 0.04,
-                ),
-                RichText
-                (
-                  text: TextSpan
+                  image: DecorationImage
                   (
-                    style: GoogleFonts.comfortaa
-                    (
-                      textStyle: TextStyle
-                      (
-                        fontWeight: FontWeight.bold,
-                        fontSize: 45,
-                        color: Colors.white,
-                      ),
-                    ),
-                    children:
-                    [
-                      TextSpan
-                      (
-                        text: 'nou',
-                        style: TextStyle
-                        (
-                          color: Color.fromRGBO(245, 197, 41, 1)
-                        ),
-                      ),
-                      TextSpan
-                      (
-                        text: 'partazer'
-                      ),
-                    ]
-                  )
-                ),
-                Container
-                (
-                  margin: EdgeInsets.fromLTRB(0, 110, 0, 0),
-                  height: screen.height * 0.23,
-                  width: screen.width * 0.7,
-                  child: RichText
-                  (
-                    text: TextSpan
-                    (
-                      style: GoogleFonts.comfortaa
-                      (
-                        textStyle: TextStyle
-                        (
-                          height: 1.2,
-                          fontSize: 29,
-                          color: Colors.white,
-                        ),
-                        ),
-                      children:
-                      [
-                        TextSpan
-                        (
-                          text: '“If you can’t feed a hundred people, then feed just ',
-                        ),
-                        TextSpan
-                        (
-                          text: 'one',
-                          style: TextStyle
-                          (
-                            color: Color.fromRGBO(245, 197, 41, 1),
-                            fontWeight: FontWeight.bold,
-                          )
-                        ),
-                        TextSpan
-                        (
-                          text: '.” - Mother Teresa',
-                        )
-                      ],
-                    ),
-                    textAlign: TextAlign.center,
+                    image: AssetImage('assets/JPEG/SplashScreen.jpg'),
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Container
+                // SpinKitSquareCircle(color:Colors.orangeAccent,size: 25.0,),
+              ),
+              Container
+              (
+                color: Color.fromRGBO(0, 0, 0, 0.3)
+              ),
+              Container
+              (
+                margin: EdgeInsets.fromLTRB
                 (
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  child:
-                    CircularProgressIndicator(),
-                )
-              ]
-            ),
+                  0,
+                  constraints.maxHeight * 0.2,
+                  0,
+                  0
+                ),
+                width: double.infinity,
+                child: Column
+                (
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children:
+                  [
+                    Image.asset
+                    (
+                      'assets/PNG/NouPartazer LogoOnly_Large.png',
+                      height: constraints.maxHeight * 0.2,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox
+                    (
+                      height: constraints.maxHeight * 0.04,
+                    ),
+                    FittedBox(
+                      child: RichText
+                      (
+                        text: TextSpan
+                        (
+                          style: GoogleFonts.comfortaa
+                          (
+                            textStyle: TextStyle
+                            (
+                              fontWeight: FontWeight.bold,
+                              fontSize: 45,
+                              color: Colors.white,
+                            ),
+                          ),
+                          children:
+                          [
+                            TextSpan
+                            (
+                              text: 'nou',
+                              style: TextStyle
+                              (
+                                color: Color.fromRGBO(245, 197, 41, 1)
+                              ),
+                            ),
+                            TextSpan
+                            (
+                              text: 'partazer'
+                            ),
+                          ]
+                        )
+                      ),
+                    ),
+
+                    Container
+                    (
+                      margin: EdgeInsets.fromLTRB
+                      (
+                        0,
+                        constraints.maxHeight * 0.1,
+                        0,
+                        0
+                      ),
+                      height: screen.height * 0.14,
+                      width: screen.width * 0.6,
+                      child: FittedBox
+                      (
+                        fit: BoxFit.fitWidth,
+                        child: RichText
+                          (
+                          text: TextSpan
+                          (
+                            style: GoogleFonts.comfortaa
+                            (
+                              textStyle: TextStyle
+                              (
+                                height: 1.2,
+                                fontSize: 29,
+                                color: Colors.white,
+                              ),
+                              ),
+                            children:
+                            [
+                              TextSpan
+                              (
+                                text: '“If you can’t feed \na hundred people,\nthen feed just ',
+                              ),
+                              TextSpan
+                              (
+                                text: 'one',
+                                style: TextStyle
+                                (
+                                  color: Color.fromRGBO(245, 197, 41, 1),
+                                  fontWeight: FontWeight.bold,
+                                )
+                              ),
+                              TextSpan
+                              (
+                                text: '.”\n- Mother Teresa',
+                              )
+                            ],
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    Container
+                    (
+                      margin: EdgeInsets.fromLTRB
+                      (
+                        0,
+                        constraints.maxHeight * 0.1,
+                        0,
+                        0
+                      ),
+                      child:
+                        CircularProgressIndicator(),
+                    )
+                  ]
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
--- NouPartazer_App/lib/main.dart
+++ NouPartazer_App/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:NouPartazer_App/Koomalai/constants.dart';
 import './SplashScreen.dart';
-void main() => runApp( MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget
 {
