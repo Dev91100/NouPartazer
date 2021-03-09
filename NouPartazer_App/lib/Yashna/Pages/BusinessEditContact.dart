3ce916f211a664bd156008d7b1b575c718455c22
atishdhu <atish-dhunnoo@hotmail.com>
2021-03-09 15:58:16 +0400

    ➕ Add Template Widget for Edit Info

--- NouPartazer_App/lib/Yashna/Pages/BusinessEditContact.dart
+++ NouPartazer_App/lib/Yashna/Pages/BusinessEditContact.dart
@@ -1,186 +1,21 @@
 import 'package:flutter/material.dart';
 
-class BusinessEditContact extends StatefulWidget {
-  final String title;
+import 'package:NouPartazer_App/Yashna/components/EditInfo.dart';
 
-  const BusinessEditContact({
-    @required this.title,
-  });
-
+class BusinessEditContact extends StatelessWidget
+{
   @override
-  _BusinessEditContactState createState() => _BusinessEditContactState();
-}
-
-class _BusinessEditContactState extends State<BusinessEditContact> {
-  get actions => null;
-
-
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-     //resizeToAvoidBottomInset: false,
-     backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(widget.title),
-          
-          
-          actions: [
-
-            IconButton(
-              icon: Icon(Icons.open_in_new),
-              onPressed: showBottomSheet,
-                
-            ),
-          ],
-          
-        ),
-      body: Center(),
-     
-      
- );
-        
-    void showBottomSheet() => showModalBottomSheet(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(8),
-          topRight: Radius.circular(8),
-      )
-    ),
-
-      context: context,
-      
-      builder: (context) => SingleChildScrollView(
-        child: Column(
-        mainAxisSize: MainAxisSize.min,
-          
-        children: <Widget> [
-           
-           IconButton(
-              icon: Icon(Icons.close),
-              //alignment: Alignment.topRight,
-              padding: EdgeInsets.only(left: 350),
-              onPressed: () {
-                //Put your code here which you want to execute on close button click.
-                Navigator.of(context).pop();
-              },
-           
-              ),
-                  
-        ListTile(
-            leading: Icon(Icons.language),
-            title: Text("WEBSITE",
-            style: TextStyle
-                    (
-                      color: Color.fromRGBO(102, 102, 102, 1),
-                  ),
-            
-            ),
-            subtitle: TextField(
-             style: TextStyle
-                    (
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                    
-                    )
-                    ),
-        
-            onTap: () => {}, 
-        ),
-          
-         ListTile(
-            leading: Icon(Icons.email_outlined),
-            title: Text("EMAIL",
-            style: TextStyle
-                    (
-                      color: Color.fromRGBO(102, 102, 102, 1),
-                  
-                    ),
-            
-            ),
-            subtitle: TextField(
-             style: TextStyle
-                    (
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                    
-                    )
-                    ),
-           onTap: () => {},
-         ),
-           
-           ListTile(
-            leading: Icon(Icons.phone_outlined),
-            title: Text("PHONE",
-            style: TextStyle
-                    (
-                      color: Color.fromRGBO(102, 102, 102, 1),
-                  
-                    ),
-            
-            ),
-            subtitle: TextField(
-             style: TextStyle
-                    (
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                    
-                    )
-                    ),
-           onTap: () => {},
-           ),
-        
-         Container(
-          child : Row(
-            children: <Widget> [
-              Padding(padding: 
-              EdgeInsets.all(50.0), ),
-             TextButton(
-              child: Text("Cancel",
-              style: TextStyle
-                    (
-                      decoration:TextDecoration.underline,
-                      color: Color.fromRGBO(102, 102, 102, 1),
-                      fontSize: 20.0,
-                         
-                    )
-                    ),
-              
-              onPressed: () {
-                //Put your code here which you want to execute on Yes button click.
-                Navigator.of(context).pop();
-              },
-              ),
-                
-              Padding(padding: 
-              EdgeInsets.all(10.0), ),
-              
-              OutlinedButton(
-              child: Text("Save",
-               style: TextStyle
-                    (
-                      color: Color.fromRGBO(245, 197, 41, 1),
-                      fontSize: 20.0,
-                      
-                    ),
-              ),
-               style: OutlinedButton.styleFrom(
-                      backgroundColor: Colors.yellow[50],
-                      side: BorderSide(color: Color.fromRGBO(245, 197, 41, 1), width: 1.5),
-                      primary: Color.fromRGBO(212, 0, 0, 1),
-                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(7))),
-                     
-                    ),
-                    
-                      onPressed: () {
-                //Put your code here which you want to execute on Cancel button click.
-                Navigator.of(context).pop();
-                
-              },
-              
-               ),
-            ],
-          )          
-           
-           ),
-        ]
-        ),
-      ));
-    
+  Widget build(BuildContext context)
+  {
+    return EditInfo
+    (
+      title: 'TEST',
+      icon1: Icons.language,
+      icon2: Icons.email_outlined,
+      icon3: Icons.phone_outlined,
+      text1: 'WEBSITE',
+      text2: 'EMAIL',
+      text3: 'PHONE',
+    );
+  }
 }
