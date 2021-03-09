338f0728b379718bc4eb7ef9c9b88214c7f8ade4
YashNa99 <singaryashna99@gmail.com>
2021-03-09 13:50:10 +0400

    Second commit for Pop Up

---
+++ NouPartazer_App/lib/Yashna/sample10 - Cancel Donation/sample10/android/app/src/main/kotlin/com/example/sample10/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sample10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ NouPartazer_App/lib/Yashna/sample9 - Delete Member/sample9/android/app/src/main/kotlin/com/example/sample6/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sample6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
