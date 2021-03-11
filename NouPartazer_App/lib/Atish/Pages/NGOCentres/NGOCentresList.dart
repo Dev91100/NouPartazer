5b395892eae441d7d4500ab7d7fc7ec7cf34a111
atishdhu <atish-dhunnoo@hotmail.com>
2021-03-11 14:33:49 +0400

    🔃 Refine EditPhotoButton.dart file to accept more parameters

---
+++ NouPartazer_App/lib/Atish/Pages/NGOCentres/NGOCentresList.dart
@@ -0,0 +1,20 @@
+import 'package:NouPartazer_App/Atish/Pages/NGOCentres/NGOCentres.dart';
+
+final List<NGOCentres> ngoStoryList =
+[
+  NGOCentres
+  (
+    profilePhoto: './assets/JPEG/Child.jpg',
+    bannerPhoto: ,
+    name: ,
+    expertise: ,
+    size: ,
+    address: ,
+    phone: ,
+    website: ,
+    description:
+  ),
+  
+
+  
+];
